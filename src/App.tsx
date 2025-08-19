b86c71d4fea51355ce58e45c309daffca0175986
Harris Escalante <11287035+harrisfoes@users.noreply.github.com>
2025-08-19 14:31:04 +0900

    added modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
 import "./App.css";
-import bgTriangle from "/src/assets/bg-triangle.svg";
-import iconRock from "/src/assets/icon-rock.svg";
-import iconPaper from "/src/assets/icon-paper.svg";
-import iconScissors from "/src/assets/icon-scissors.svg";
-import Button from "./components/Button";
+import { useState } from "react";
 import Header from "./components/Header";
+import Gamebody from "./components/Gamebody";
+import rules from "./assets/image-rules.svg";
 
 function App() {
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <div className="flex-col justify-center justify-items-center text-gray-200 font-bold min-h-screen overflow-auto bg-[radial-gradient(circle_at_center,_hsl(214,47%,23%),_hsl(237,49%,15%))]">
-        <Header score={0} /> 
-        <div className="gameBody mt-24 relative flex justify-center my-10">
-          <div className="triangle flex justify-center my-20">
-            <img src={bgTriangle} className="w-3/4" />
-          </div>
-          <div className="rpc-buttons absolute pt-4 px-8 w-[375px]">
-            <div className="flex justify-between">
-              <Button icon={iconPaper} color="bg-blue-500" />
-              <Button icon={iconScissors} color="bg-yellow-500" />
-            </div>
-            <div className="flex justify-center pt-6">
-              <Button icon={iconRock} color="bg-red-500" />
-            </div>
-          </div>
-        </div>
+        <Header score={0} />
+        <Gamebody />
         <div className="rules-container mt-16 w-full flex justify-center ">
-        <button className="uppercase border-2 border-gray-500 text-gray-200 rounded-lg py-2 px-10 active:scale-90">Rules </button>
+          <button
+            onClick={() => setOpen(true)}
+            className="uppercase border-2 border-gray-500 text-gray-200 rounded-lg py-2 px-10 active:scale-95"
+          >
+            Rules{" "}
+          </button>
+          {/*Parent visibler*/}
+          <div
+            onClick={() => setOpen(false)}
+            className={`fixed inset-0 flex justify-center items-center bg-black/20 transition-colors ${open ? "visible" : "invisible"}`}
+          >
+            <div onClick={(e) => e.stopPropagation()}
+              className={`flex-col justify-evenly h-3/4 bg-white text-slate-800 shadow p-6 transition-all rounded-xl ${open ? "scale-100 opacity-100" : "scale-90 opacity-0"}`}>
+             <div className="text-center uppercase">Rules</div> 
+              <img src={rules} />
+              <button className="absolute top-2 right-2" onClick={()=>setOpen(false)}>
+                X
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
---
+++ src/components/Gamebody.tsx
@@ -0,0 +1,30 @@
+
+import bgTriangle from "/src/assets/bg-triangle.svg";
+
+import iconRock from "/src/assets/icon-rock.svg";
+import iconPaper from "/src/assets/icon-paper.svg";
+import iconScissors from "/src/assets/icon-scissors.svg";
+import Button from "./Button";
+
+const Gamebody = () => {
+  return (
+    <>
+      <div className="gameBody mt-24 relative flex justify-center my-10">
+        <div className="triangle flex justify-center my-20">
+          <img src={bgTriangle} className="w-3/4" />
+        </div>
+        <div className="rpc-buttons absolute pt-4 px-8 w-[375px]">
+          <div className="flex justify-between">
+            <Button icon={iconPaper} color="bg-blue-500" />
+            <Button icon={iconScissors} color="bg-yellow-500" />
+          </div>
+          <div className="flex justify-center pt-6">
+            <Button icon={iconRock} color="bg-red-500" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Gamebody;
